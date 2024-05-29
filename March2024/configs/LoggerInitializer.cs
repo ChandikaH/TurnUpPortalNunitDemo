b4505a9b67fbe75c3123b569ee2d7cddc90c8cc8
Chandika Herath <chandikasbh@gmail.com>
2024-05-29 21:21:44 +1200

    Removed config folder

--- March2024/configs/LoggerInitializer.cs
+++
@@ -1,9 +0,0 @@
-﻿using log4net.Config;
-
-public static class LoggerInitializer
-{
-    public static void Initialize()
-    {
-        XmlConfigurator.Configure(new System.IO.FileInfo("log4net.config"));
-    }
-}
